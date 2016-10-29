28ca260708beee5ba7495e19995b3c2d742018d3
Малков Матвей <xaxalox2@gmail.com>
2016-10-30 00:51:24 +0500

    Update app.py

--- pyramid/app.py
+++ pyramid/app.py
@@ -2,10 +2,18 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
+
+def index(request):
+    return Response("""<a href="index.html">asd</a> <br> <a href="http://localhost:8000/index.html">zxc</a>""")
+def about(request):
+    return Response("""<a href="about/aboutme.html">asd1</a> <br> <a href="http://localhost:8000/about/aboutme.html">zxc1</a>""")
+
 if __name__ == '__main__':
     config = Configurator()
-    config.scan("views")
+    config.add_route('index', '/')
+    config.add_view(index, route_name='index')
+    config.add_route('about', 'about')
+    config.add_view(about, route_name='about')
     app = config.make_wsgi_app()
-
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
